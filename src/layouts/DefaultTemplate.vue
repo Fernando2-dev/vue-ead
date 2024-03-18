e9d5c708e0c341d90b3e53fa1e9fae7a908fc2c3
Fernando Henrique da Silva <109765433+Fernando2-dev@users.noreply.github.com>
2024-03-18 17:01:34 -0300

    criando a página home e a página de supports

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+     <header-component/>
+
+     <section>
+        <router-view></router-view>
+     </section>
+
+     <footer-component/>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent"
+import FooterComponent from "./components/FooterComponent"
+
+ export default {
+    name : "DefaultTemplate",
+    components: {
+        HeaderComponent,
+        FooterComponent
+    }
+ }
+</script>
---
+++ src/layouts/components/FooterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer id="header">
+            <span class="copyright fontSmall">
+                Todos os Direitos reservados - <b>Especializa TI</b>
+            </span>
+        </footer>
+    </template>
+    
+    <script>
+     export default {
+        name: "header-component"
+     }
+    </script>
+    
+    <style>
+    </style>
