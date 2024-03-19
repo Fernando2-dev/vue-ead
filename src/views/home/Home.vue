c1acdef0039b315de85ad78d7270052ef1142da1
Fernando Henrique da Silva <109765433+Fernando2-dev@users.noreply.github.com>
2024-03-19 15:01:44 -0300

    criando a página de listagem de cursos com components separado para a organização do código

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="home">
-    Listagem dos cursos
-    </div>
+  <div class="pageTitle">
+    <span class="title">Meus Cursos</span>
+    <span class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </div>
+
+  <div class="content">
+    <ul class="listCourses">
+      <card-couse 
+      :title="'curso de laravel'"
+      :image="''"
+      :bgName="'laravel'"
+      ></card-couse>
+    </ul>
+  </div>
 </template>
 
 <script>
+import CardCouse from "./components/CardCouse.vue"
 
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    CardCouse
+  }
 }
 </script>
